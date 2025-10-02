53aab509fa04d8d3748ad92a5b2d626a9aa7f7af
1Twist <128860300+1Twist@users.noreply.github.com>
2025-10-02 17:52:41 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+import { NpcWeaponChoice } from "./NpcWeaponChoice";
+
+export function App() {
+  return (
+    <div className="container mx-auto p-8 text-center relative z-10">
+      <NpcWeaponChoice />
+    </div>
+  
+);
+  
+}
+
+export default App;
+
+
---
+++ src/components/horse-icon.tsx
@@ -0,0 +1,6 @@
+import horseIcon from "../assets/horse.svg";
+export const HorseIcon = () => { 
+  return (
+    <img className="absolute top-0 left-0 size-10" src={horseIcon} alt="Horse Icon" />
+  );
+};
